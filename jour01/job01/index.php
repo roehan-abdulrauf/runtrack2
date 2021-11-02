5dffe33247c83dbcf4b607fd8b11b7fdc3b8e37e
roehan-abdulrauf <91737334+roehan-abdulrauf@users.noreply.github.com>
2021-11-02 14:56:03 +0100

    job01
    édition des codes demander dans le sujet pour la page index

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -7,11 +7,14 @@
 </head>
 <body>
 <?php
-$str = "LaPlateforme";
-$str2 = "Vive";
+$str = "LaPlateforme ";
+$str2 = "Vive ";
 $str3 = "!";
-echo $str.$str2.$str3;
-$val = 6;
+echo "$str2 $str $str3";
+echo "<br>";
+$val = 6 ;
+echo $val;
+echo "<br>";
 $val2 = 4;
 echo $val + $val2;
 ?>
