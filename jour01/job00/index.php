72fa766ac3da90df0e0c63b10149ef52056bf255
roehan-abdulrauf <91737334+roehan-abdulrauf@users.noreply.github.com>
2021-11-02 15:39:36 +0100

    job00 et job01
    rendre le code plus propres

--- jour01/job00/index.php
+++ jour01/job00/index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <title>job00</title>
 </head>
 <body>
-<?php
-echo "Hello LaPlateforme!"
-?>
+    <?php
+    echo "Hello LaPlateforme!"
+    ?>
 </html>
 </body>
--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <title>job01</title>
 </head>
 <body>
-<?php
-$str = "LaPlateforme ";
-$str2 = "Vive ";
-$str3 = "!";
-echo "$str2 $str $str3";
-echo "<br>";
-$val = 6 ;
-echo $val;
-echo "<br>";
-$val2 = 4;
-echo $val + $val2;
-?>
-</html>
+    <?php
+
+    $str = "LaPlateforme ";
+    $str2 = "Vive ";
+    $str3 = "!";
+    echo "$str2 $str $str3";
+
+    echo "<br>";
+    $val = 6;
+    echo $val;
+
+    echo "<br>";
+    $val2 = 4;
+    echo $val + $val2;
+
+    ?>
 </body>
+
+</html>
