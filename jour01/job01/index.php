2e6aec4baaf82d1200b745db11eda3a1df76e1f3
roehan-abdulrauf <91737334+roehan-abdulrauf@users.noreply.github.com>
2021-11-02 14:38:03 +0100

    dossier jour01 et dossier job01
    Création du dossier jour01 et du dossier jod01 et intégration du fichier index.php

---
+++ jour01/job01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+</head>
+<body>
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo $str.$str2.$str3;
+$val = 6;
+$val2 = 4;
+echo $val + $val2;
+?>
+</html>
+</body>
