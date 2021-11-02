6c66b0f6c13a9f65341fbfd2da044744af435fdb
roehan-abdulrauf <91737334+roehan-abdulrauf@users.noreply.github.com>
2021-11-02 16:09:51 +0100

    job02
    édition du code de l'index avec deux type de commentaire

---
+++ jour01/job02/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>job00</title>
+</head>
+
+<body>
+    <?php
+    
+    //pour afficher hello LaPlateforme! j'ai d'abord utilisé la manière simple avec echo sans variable
+    echo nl2br("Hello LaPlateforme!\n");
+
+    /*pour afficher le deuxième hello LaPlateforme!
+    j'utilise une variable que je nomme job02 puis
+     je l'affiche avec echo */
+    $job02 = "Hello LaPlateforme!";
+    echo $job02;
+
+    ?>
+</body>
+
+</html>
